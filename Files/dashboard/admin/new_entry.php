5c4f350334b123ccc9e9c4a019888c9fee82a12d
Rocktim <rocktim53@gmail.com>
2018-05-30 11:26:18 +0530

    new_entry design done

--- Files/dashboard/admin/new_entry.php
+++ Files/dashboard/admin/new_entry.php
@@ -13,6 +13,7 @@ page_protect();
     <script type="text/javascript" src="../../js/Script.js"></script>
     <link rel="stylesheet" href="../../css/dashMain.css">
     <link rel="stylesheet" type="text/css" href="../../css/entypo.css">
+    <link href="a1style.css" type="text/css" rel="stylesheet">
 
 </head>
       <body class="page-body  page-fade" onload="initializeMember()">
@@ -72,75 +73,71 @@ page_protect();
 					
 				</div>
 
-		<h3>New Netry</h3>
-
-		<hr />
 		
-		<form action="new_submit.php" method="POST">
-					
-			<div class="form_group">
-				<label for="memberid">Memebership ID : </label>
-				<input type="text" name="m_id" value="<?php echo time(); ?>" readonly required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="username">Name : </label>
-				<input type="text" name="u_name" id="username" required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="streetname">Street Name : </label>
-				<input type="text" name="street_name" id="streetname" required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="city">City : </label>
-				<input type="text" name="city" id="city" required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="zipcode">Zipcode : </label>
-				<input type="number" name="zipcode" id="zipcode" maxlength="6" required />	
-			</div>
-
-			<div class="form_group">
-				<label for="state">State : </label>
-				<input type="text" name="state" id="state" required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="gender">Gender : </label>
-				<select name="gender" id="gender" required>
+        
+        
+        <div class="a1-container a1-small a1-padding-32" style="margin-top:2px; margin-bottom:2px;">
+        <div class="a1-card-8 a1-light-gray" style="width:500px; margin:0 auto;">
+		<div class="a1-container a1-dark-gray a1-center">
+        	<h6>NEW ENTRY</h6>
+        </div>
+       <form id="form1" name="form1" method="post" class="a1-container" action="new_submit.php">
+         <table width="100%" border="0" align="center">
+         <tr>
+           <td height="35"><table width="100%" border="0" align="center">
+           	 <tr>
+           	   <td height="35">MEMBERSHIP ID:</td>
+           	   <td height="35"><input type="text" name="m_id" value="<?php echo time(); ?>" readonly required/></td>
+         	   </tr>
+			   
+			   <tr>
+               <td height="35">NAME:</td>
+               <td height="35"><input name="u_name" id="username" size="30" required/></td>
+             </tr>
+             <tr>
+               <td height="35">STREET NAME:</td>
+               <td height="35"><input  name="street_name" id="streetname"  size="30" required/></td>
+             </tr>
+             <tr>
+               <td height="35">CITY:</td>
+               <td height="35"><input <input type="text" name="city" id="city" required/ size="30"></td>
+             </tr>
+             <tr>
+               <td height="35">ZIPCODE:</td>
+               <td height="35"><input type="number" name="zipcode" id="zipcode" maxlength="6" required / size="30"></td>
+             </tr>
+            <tr>
+               <td height="35">STATE:</td>
+               <td height="35"><input type="text" name="state" id="state" required/ size="30"></td>
+             </tr>
+            <tr>
+               <td height="35">GENDER:</td>
+               <td height="35"><select name="gender" id="gender" required>
 
 					<option value="">--Please Select--</option>
 					<option value="Male">Male</option>
 					<option value="Female">Female</option>
-				</select>
-			</div>
-
-			<div class="form_group">
-				<label for="dob">Date Of Birth : </label>
-				<input type="date" name="dob" id="dob" required>
-			</div>
-
-			<div class="form_group">
-				<label for="mobile">Phone No : </label>
-				<input type="number" name="mobile" id="mobile" maxlength="10" required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="email">Email ID : </label>
-				<input type="email" name="email" id="email" required/>	
-			</div>
-
-			<div class="form_group">
-				<label for="jdate">Joining Date : </label>
-				<input type="date" name="jdate" id="jdate" required>
-			</div>
-					
-			<div class="form_group">
-				<label for="plan">Plan : </label>
-				<select name="plan" id="plan" required>
+				</select></td>
+             </tr>
+            <tr>
+               <td height="35">DATE OF BIRTH:</td>
+               <td height="35"><input type="date" name="dob" id="dob" required/ size="30"></td>
+             </tr>
+			 <tr>
+               <td height="35">PHONE NO:</td>
+               <td height="35"><input type="number" name="mobile" id="mobile" maxlength="10" required/ size="30"></td>
+             </tr>
+			  <tr>
+               <td height="35">EMAIL ID:</td>
+               <td height="35"><input type="email" name="email" id="email" required/ size="30"></td>
+             </tr>
+			 <tr>
+               <td height="35">JOINING DATE:</td>
+               <td height="35"><input type="date" name="jdate" id="jdate" required size="30"></td>
+             </tr>
+             <tr>
+               <td height="35">PLAN:</td>
+               <td height="35"><select name="plan" id="plan" required>
 					<option value="">--Please Select--</option>
 					<?php
 						$query="select * from plan";
@@ -153,15 +150,25 @@ page_protect();
 
 					?>
 					
-				</select>
-
-			</div>
-
-			<div class="form_group">
-				<button type="submit">Register</button>
-			</div>
-
-		</form>
+				</select></td>
+             </tr>
+            
+             <tr>
+             <tr>
+               <td height="35">&nbsp;</td>
+               <td height="35"><input class="a1-btn a1-blue" type="submit" name="submit" id="submit" value="Register" >
+                 <input class="a1-btn a1-blue" type="reset" name="reset" id="reset" value="Reset"></td>
+             </tr>
+           </table></td>
+         </tr>
+         </table>
+       </form>
+    </div>
+    </div>   
+        
+        
+        
+        
 			<?php include('footer.php'); ?>
     	</div>
 
