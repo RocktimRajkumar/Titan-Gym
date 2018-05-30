c4f89211288b73aede3d6ad8a1b052a026b5141a
Rocktim <rocktim53@gmail.com>
2018-05-30 11:25:22 +0530

    new_entry and new_submit modified

--- Files/dashboard/admin/new_entry.php
+++ Files/dashboard/admin/new_entry.php
@@ -80,38 +80,39 @@ page_protect();
 					
 			<div class="form_group">
 				<label for="memberid">Memebership ID : </label>
-				<input type="text" name="m_id" value="<?php echo time(); ?>" readonly/>	
+				<input type="text" name="m_id" value="<?php echo time(); ?>" readonly required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="username">Name : </label>
-				<input type="text" name="u_name" id="username"/>	
+				<input type="text" name="u_name" id="username" required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="streetname">Street Name : </label>
-				<input type="text" name="street_name" id="streetname"/>	
+				<input type="text" name="street_name" id="streetname" required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="city">City : </label>
-				<input type="text" name="city" id="city"/>	
+				<input type="text" name="city" id="city" required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="zipcode">Zipcode : </label>
-				<input type="number" name="zipcode" id="zipcode" max="6" />	
+				<input type="number" name="zipcode" id="zipcode" maxlength="6" required />	
 			</div>
 
 			<div class="form_group">
 				<label for="state">State : </label>
-				<input type="text" name="state" id="state"/>	
+				<input type="text" name="state" id="state" required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="gender">Gender : </label>
-				<select name="gender" id="gender">
+				<select name="gender" id="gender" required>
 
+					<option value="">--Please Select--</option>
 					<option value="Male">Male</option>
 					<option value="Female">Female</option>
 				</select>
@@ -119,34 +120,34 @@ page_protect();
 
 			<div class="form_group">
 				<label for="dob">Date Of Birth : </label>
-				<input type="date" name="dob" id="dob" max="1990-12-31">
+				<input type="date" name="dob" id="dob" required>
 			</div>
 
 			<div class="form_group">
 				<label for="mobile">Phone No : </label>
-				<input type="number" name="mobile" id="mobile"/>	
+				<input type="number" name="mobile" id="mobile" maxlength="10" required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="email">Email ID : </label>
-				<input type="email" name="email" id="email"/>	
+				<input type="email" name="email" id="email" required/>	
 			</div>
 
 			<div class="form_group">
 				<label for="jdate">Joining Date : </label>
-				<input type="date" name="jdate" id="jdate" max="1990-12-31">
+				<input type="date" name="jdate" id="jdate" required>
 			</div>
 					
 			<div class="form_group">
 				<label for="plan">Plan : </label>
-				<select name="plan" id="plan">
+				<select name="plan" id="plan" required>
 					<option value="">--Please Select--</option>
 					<?php
 						$query="select * from plan";
 						$result=mysqli_query($con,$query);
 						if(mysqli_affected_rows($con)!=0){
 							while($row=mysqli_fetch_row($result)){
-								echo "<option value=".$row[0].">".$row[1]."</option>";
+								echo "<option value=".$row[0].">".$row[2]."</option>";
 							}
 						}
 
--- titangym.sql
+++ titangym.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50714
  File Encoding         : 65001
 
- Date: 26/05/2018 07:47:05
+ Date: 29/05/2018 20:58:35
 */
 
 SET NAMES utf8mb4;
@@ -22,7 +22,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `address`;
 CREATE TABLE `address`  (
-  `id` int(5) NULL DEFAULT NULL,
+  `id` int(12) NULL DEFAULT NULL,
   `streetName` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `state` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `city` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
@@ -51,13 +51,18 @@ INSERT INTO `admin` VALUES ('admin1', 'admin1', 'admin1');
 DROP TABLE IF EXISTS `enrolls_to`;
 CREATE TABLE `enrolls_to`  (
   `pid` int(5) NOT NULL,
-  `id` int(5) NOT NULL,
+  `id` int(12) NOT NULL,
   `paid_date` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `expire` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `renewal` varchar(4) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  INDEX `pid`(`pid`) USING BTREE,
-  INDEX `id`(`id`) USING BTREE
+  INDEX `planid`(`pid`) USING BTREE,
+  INDEX `userID`(`id`) USING BTREE
 ) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of enrolls_to
+-- ----------------------------
+INSERT INTO `enrolls_to` VALUES (1, 1, '1212', '112', '112');
 
 -- ----------------------------
 -- Table structure for health_status
@@ -65,34 +70,40 @@ CREATE TABLE `enrolls_to`  (
 DROP TABLE IF EXISTS `health_status`;
 CREATE TABLE `health_status`  (
   `hid` int(5) NOT NULL AUTO_INCREMENT,
-  `calorie` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `height` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `weight` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `fat` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `remarks` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `id` int(5) NULL DEFAULT NULL,
-  PRIMARY KEY (`hid`) USING BTREE,
-  INDEX `id`(`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+  `calorie` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `height` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `weight` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `fat` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `remarks` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `id` int(12) NOT NULL,
+  PRIMARY KEY (`hid`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 13 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for plan
 -- ----------------------------
 DROP TABLE IF EXISTS `plan`;
 CREATE TABLE `plan`  (
-  `pid` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `pid` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `planName` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `validity` int(8) NOT NULL,
+  `description` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `validity` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `amount` int(10) NOT NULL,
-  PRIMARY KEY (`pid`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of plan
+-- ----------------------------
+INSERT INTO `plan` VALUES (1, 'XFJGH', 'Summer Plan', 'well plan', '3 months', 2500);
 
 -- ----------------------------
 -- Table structure for timetable
 -- ----------------------------
 DROP TABLE IF EXISTS `timetable`;
 CREATE TABLE `timetable`  (
-  `tid` int(5) NOT NULL AUTO_INCREMENT,
+  `tid` int(12) NOT NULL AUTO_INCREMENT,
   `tname` varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `day1` varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `day2` varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
@@ -111,15 +122,16 @@ CREATE TABLE `timetable`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `userid` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `username` varchar(40) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `gender` varchar(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `mobile` int(12) NOT NULL,
+  `mobile` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `email` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `dob` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `joining_date` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `email`(`email`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
