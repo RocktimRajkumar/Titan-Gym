09865889839d3d517ddd6aa4aff2ddab0de7ccc9
Rocktim <rocktim53@gmail.com>
2018-06-01 17:36:14 +0530

    view_mem html done

---
+++ Files/dashboard/admin/view_mem.php
@@ -0,0 +1,185 @@
+﻿<?php
+require '../../include/db_conn.php';
+page_protect();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Titan Gym</title>
+    <link rel="stylesheet" href="../../css/style.css"  id="style-resource-5">
+    <script type="text/javascript" src="../../js/Script.js"></script>
+    <link rel="stylesheet" href="../../css/dashMain.css">
+    <link rel="stylesheet" type="text/css" href="../../css/entypo.css">
+
+</head>
+    <body class="page-body  page-fade" onload="initializeMember()">
+
+    	<div class="page-container sidebar-collapsed" id="navbarcollapse">	
+	
+		<div class="sidebar-menu">
+	
+			<header class="logo-env">
+			
+			<!-- logo -->
+			<div class="logo">
+				<a href="main.php">
+					<img src="../../images/logo.png" alt="" width="192" height="80" />
+				</a>
+			</div>
+			
+					<!-- logo collapse icon -->
+					<div class="sidebar-collapse" onclick="collapseSidebar()">
+				<a href="#" class="sidebar-collapse-icon with-animation"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+					<i class="entypo-menu"></i>
+				</a>
+			</div>
+							
+			
+		
+			</header>
+    		<?php include('nav.php'); ?>
+    	</div>
+
+
+    		<div class="main-content">
+		
+				<div class="row">
+					
+					<!-- Profile Info and Notifications -->
+					<div class="col-md-6 col-sm-8 clearfix">	
+							
+					</div>
+					
+					
+					<!-- Raw Links -->
+					<div class="col-md-6 col-sm-4 clearfix hidden-xs">
+						
+						<ul class="list-inline links-list pull-right">
+
+							<li>Welcome <?php echo $_SESSION['full_name']; ?> 
+							</li>								
+						
+							<li>
+								<a href="logout.php">
+									Log Out <i class="entypo-logout right"></i>
+								</a>
+							</li>
+						</ul>
+						
+					</div>
+					
+				</div>
+
+		<h3>Sam's Slim Gym</h3>
+
+		<hr />
+		
+		<table class="table table-bordered datatable" id="table-1">
+			<thead>
+				<tr>
+					<th>Membership Expiry</th>
+					<th>Member ID</th>
+					<th>Name</th>
+					<th>Contact</th>
+					<th>E-Mail</th>
+					<th>Gender</th>
+					<th>Joining Date</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+				<tbody>
+
+						<?php
+							$query  = "select * from users ORDER BY joining_date";
+							//echo $query;
+							$result = mysqli_query($con, $query);
+							$sno    = 1;
+
+							if (mysqli_affected_rows($con) != 0) {
+							    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+							        $uid   = $row['userid'];
+							        $query1  = "select * from enrolls_to WHERE uid='$uid' AND renewal='yes'";
+							        $result1 = mysqli_query($con, $query1);
+							        if (mysqli_affected_rows($con) == 1) {
+							            while ($row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC)) {
+							                
+							                
+							                echo "<tr><td>" . $row1['expire'] . "</td>";
+							                
+							                echo "<td>" . $row['userid'] . "</td>";
+
+							                echo "<td>" . $row['username'] . "</td>";
+
+							                echo "<td>" . $row['mobile'] . "</td>";
+
+							                echo "<td>" . $row['email'] . "</td>";
+
+							                echo "<td>" . $row['gender'] . "</td>";
+
+							                echo "<td>" . $row['joining_date'] ."</td>";
+							                
+							                $sno++;
+							                
+							                echo "<td><form action='read_member.php' method='post'><input type='hidden' name='name' value='" . $uid . "'/><input type='submit' value='View History ' class='btn btn-info'/></form><form action='edit_member.php' method='post'><input type='hidden' name='name' value='" . $uid . "'/><input type='submit' value='Edit' class='btn btn-warning'/></form><form action='del_member.php' method='post' onSubmit='return ConfirmDelete();'><input type='hidden' name='name' value='" . $uid . "'/><input type='submit' value='Delete ' class='btn btn-danger'/></form></td></tr>";
+							                $msgid = 0;
+							            }
+							        }
+							    }
+							}
+						?>									
+					</tbody>
+				</table>
+
+<script type="text/javascript">
+	jQuery(document).ready(function($)
+	{
+		$("#table-1").dataTable({
+			"sPaginationType": "bootstrap",
+			"aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+			"bStateSave": true
+		});
+		
+		$(".dataTables_wrapper select").select2({
+			minimumResultsForSearch: -1
+		});
+	});
+</script>
+
+			<?php include('footer.php'); ?>
+    	</div>
+
+     <script src="../../neon/js/gsap/main-gsap.js" id="script-resource-1"></script>
+    <script src="../../neon/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js" id="script-resource-2"></script>
+    <script src="../../neon/js/bootstrap.min.js" id="script-resource-3"></script>
+    <script src="../../neon/js/joinable.js" id="script-resource-4"></script>
+    <script src="../../neon/js/resizeable.js" id="script-resource-5"></script>
+    <script src="../../neon/js/neon-api.js" id="script-resource-6"></script>
+    <script src="../../neon/js/jquery.validate.min.js" id="script-resource-7"></script>
+    <script src="../../neon/js/neon-login.js" id="script-resource-8"></script>
+    <script src="../../neon/js/neon-custom.js" id="script-resource-9"></script>
+    <script src="../../neon/js/neon-demo.js" id="script-resource-10"></script>
+
+	<link rel="stylesheet" href="../../neon/js/select2/select2-bootstrap.css"  id="style-resource-1">
+	<link rel="stylesheet" href="../../neon/js/select2/select2.css"  id="style-resource-2">
+
+	<script src="../../neon/js/jquery.dataTables.min.js" id="script-resource-7"></script>
+	<script src="../../neon/js/dataTables.bootstrap.js" id="script-resource-8"></script>
+	<script src="../../neon/js/select2/select2.min.js" id="script-resource-9"></script>
+
+        <script type="text/javascript">
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1");
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+var spryselect1 = new Spry.Widget.ValidationSelect("spryselect1");
+var sprytextfield3 = new Spry.Widget.ValidationTextField("sprytextfield3");
+var sprytextfield4 = new Spry.Widget.ValidationTextField("sprytextfield4");
+var spryselect2 = new Spry.Widget.ValidationSelect("spryselect2");
+    </script>
+    </body>
+</html>
+
+
+
+
+
