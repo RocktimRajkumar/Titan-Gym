0b62c9f3f957357d1ba17c686bc46aff033d046b
Rocktim <rocktim53@gmail.com>
2018-06-14 22:46:48 +0530

    new plan record not added in time table

--- Files/dashboard/admin/submit_plan_new.php
+++ Files/dashboard/admin/submit_plan_new.php
@@ -14,30 +14,15 @@ page_protect();
    
 
 	 if(mysqli_query($con,$query)==1){
-        //Inserting plan id into timetable table
-        $query1="insert into timetable(pid) values('$planid')";
-        if(mysqli_query($con,$query1)==1){
+        
         echo "<head><script>alert('PLAN Added ');</script></head></html>";
         echo "<meta http-equiv='refresh' content='0; url=new_plan.php'>";
-        }
-        else{
-        echo "<head><script>alert('NOT SUCCESSFUL, Check Again');</script></head></html>";
-        echo "error".mysqli_error($con);
-        //Delete record from plan if failed to insert into timetable table
-        $query3 = "DELETE FROM plan WHERE pid='$planid'";
-                mysqli_query($con,$query3);
+       
       }
 
-    }
     else{
         echo "<head><script>alert('NOT SUCCESSFUL, Check Again');</script></head></html>";
         echo "error".mysqli_error($con);
       }
-
-	
-	
-	
-	
-	
 
 ?>
