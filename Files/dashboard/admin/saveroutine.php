08f4315141c8eb18db0262f7ec070339b8a7cbaf
Rocktim <rocktim53@gmail.com>
2018-06-15 16:01:12 +0530

    saveroutine save to database done

--- Files/dashboard/admin/saveroutine.php
+++ Files/dashboard/admin/saveroutine.php
@@ -13,12 +13,16 @@
 		
 		
 		
-		$sql="insert into timetable values('$rname','$day1','$day2','$day3','$day4','$day5','$day6')";
+		$sql="insert into timetable(tname,day1,day2,day3,day4,day5,day6) values('$rname','$day1','$day2','$day3','$day4','$day5','$day6')";
 	
 		$result=mysqli_query($con,$sql);
 		if($result){	
 		
-			echo "<head><script>alert('Member Added Failed');</script></head></html>";
+			echo "<head><script>alert('Routine Added');</script></head></html>";
+			echo "<meta http-equiv='refresh' content='0; url=addroutine.php'>";
+		}else{
+			echo "<head><script>alert('Routine Added Failed');</script></head></html>";
+			echo mysqli_error($con);
 		}
 	
 	
